6f419d459db300b76c978433f4d191f3c66ab54a
farhana <fana@rntechnologies.com.my>
2025-12-22 17:54:53 +0800

    add new db

--- eassets_db.sql
+++ eassets_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2025 at 02:29 AM
+-- Generation Time: Dec 22, 2025 at 10:52 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `asset_categories`
+--
+
+CREATE TABLE `asset_categories` (
+  `id` int(11) NOT NULL,
+  `category_name` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `asset_categories`
+--
+
+INSERT INTO `asset_categories` (`id`, `category_name`, `description`, `created_at`) VALUES
+(1, 'Laptop', 'All types of laptops used by staff and students', '2025-12-22 08:10:24'),
+(2, 'Monitor', '1234', '2025-12-22 08:48:44'),
+(3, 'pc', 'we', '2025-12-22 09:16:14');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -36,24 +58,23 @@ CREATE TABLE `users` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE asset_categories (
-    category_id INT AUTO_INCREMENT PRIMARY KEY,
-    category_name VARCHAR(100) NOT NULL,
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `fullname`, `email`, `userid`, `password`, `created_at`) VALUES
-(1, 'NUR SYAZLEEN ', 'nursyazleen28032003@gmail.com', 'AI220099', '$2y$10$i6lkZhrIuToUs6hF1jGq2uOwqQbr7Q/zqNnbuoe6OlO5C91.womUK', '2025-12-21 06:39:04');
+(1, 'NUR SYAZLEEN ', 'nursyazleen28032003@gmail.com', 'AI220099', '$2y$10$i6lkZhrIuToUs6hF1jGq2uOwqQbr7Q/zqNnbuoe6OlO5C91.womUK', '2025-12-21 06:39:04'),
+(4, 'naz', 'naz@gmail.com', 'ai12', '$2y$10$rjKc.caLh2LvOMDmNrowa.3mFZypDwc6ucmX78aARV3IlWnN.DFRC', '2025-12-22 06:33:39');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `asset_categories`
+--
+ALTER TABLE `asset_categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -68,10 +89,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `asset_categories`
+--
+ALTER TABLE `asset_categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
